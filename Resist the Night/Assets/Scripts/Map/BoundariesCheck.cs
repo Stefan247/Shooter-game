51f204ecf107a801174f3b14ea2f464d980694f9
Stefan247 <34254467+Stefan247@users.noreply.github.com>
2020-02-22 00:26:54 +0200

    ver 0.1

--- Resist the Night/Assets/Scripts/Map/BoundariesCheck.cs
+++ Resist the Night/Assets/Scripts/Map/BoundariesCheck.cs
@@ -6,10 +6,8 @@ namespace Map
     {
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (!collision.gameObject.CompareTag("Player"))
-            {
-                Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
-            }
+            if (collision.gameObject.CompareTag("Player")) return;
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
         }
     }
 }
