0fc04c7452c73629a40db6269807429ce9ab45ce
Stefan247 <34254467+Stefan247@users.noreply.github.com>
2020-02-22 14:03:28 +0200

    New menu and camera shake

---
+++ Resist the Night/Assets/Scripts/CameraScripts/CameraShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CameraScripts
+{
+    public class CameraShake : MonoBehaviour
+    {
+        private const float Zero = 0f;
+        private const float One = 1f;
+
+        public IEnumerator Shake(float duration, float magnitude)
+        {
+            Vector3 originalPos = transform.localPosition;
+            var elapsed = Zero;
+
+            while (elapsed < duration)
+            {
+                var x = Random.Range(-One, One) * magnitude;
+                var y = Random.Range(-One, One) * magnitude;
+                
+                transform.localPosition = new Vector3(x, y, originalPos.z);
+                elapsed += Time.deltaTime;
+                
+                yield return null;
+            }
+            transform.localPosition = originalPos;
+        }
+    }
+}
--- Resist the Night/Assets/Scripts/PlayerScripts/ShootingScript.cs
+++ Resist the Night/Assets/Scripts/PlayerScripts/ShootingScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using CameraScripts;
 
 namespace PlayerScripts
 {
@@ -6,14 +7,19 @@ namespace PlayerScripts
     {
         public Transform firePoint;
         public GameObject bulletPrefab;
+        public CameraShake cameraShake;
         public float bulletForce = 20f;
-    
+        
+        private float screenShakeMagnitude = 0.03f;
+        private float screenShakeDuration = 0.15f;
+
         private void Update()
         {
             if (!Input.GetButtonDown("Fire1")) return;
             var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             var rigidBody = bullet.GetComponent<Rigidbody2D>();
             rigidBody.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+            StartCoroutine(cameraShake.Shake(screenShakeDuration, screenShakeMagnitude));
         }
     }
 }
