266f3c15a62ae5c351b015b75538329dc72e22bb
Stefan247 <34254467+Stefan247@users.noreply.github.com>
2020-02-22 17:30:09 +0200

    added holdtoShoot

--- Resist the Night/Assets/Scripts/PlayerScripts/ShootingScript.cs
+++ Resist the Night/Assets/Scripts/PlayerScripts/ShootingScript.cs
@@ -9,19 +9,40 @@ namespace PlayerScripts
         public GameObject bulletPrefab;
         public CameraShake cameraShake;
         public float bulletForce = 20f;
-        /*
-         * private float screenShakeMagnitude = 0.03f;
-         * private float screenShakeDuration = 0.15f;
-         */
+
+        private float fireSpeed = .2f;
+        private float fireRate = .2f;
+        private const float Zero = 0f;
+        // private float screenShakeMagnitude = 0.1f;
+        // private float screenShakeDuration = 0.2f;
+         
         private void Update()
         {
-            if (!Input.GetButtonDown("Fire1")) return;
+            if (Input.GetButtonDown("Fire1")) // if not holding 
+            {
+                Shoot();
+            }
+
+            if (Input.GetMouseButton(0)) // if holding 
+            {
+                if (fireRate >= fireSpeed)
+                {
+                    // StartCoroutine(cameraShake.Shake(screenShakeDuration, screenShakeMagnitude));
+                    Shoot();
+                }
+                else
+                {
+                    fireRate += Time.deltaTime;
+                }
+            }
+        }
+
+        private void Shoot()
+        {
             var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             var rigidBody = bullet.GetComponent<Rigidbody2D>();
             rigidBody.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-            /*
-             * StartCoroutine(cameraShake.Shake(screenShakeDuration, screenShakeMagnitude));
-             */
+            fireRate = Zero;
         }
     }
 }
