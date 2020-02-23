3afe99c7b20fcc55f487c6165c0a6e450b5de49e
Stefan247 <34254467+Stefan247@users.noreply.github.com>
2020-02-23 13:20:25 +0200

    added pause menu + UI fix

---
+++ Resist the Night/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MenuScripts
+{
+    public class PauseMenu : MonoBehaviour
+    {
+
+        public static bool IsPaused;
+        public GameObject pauseMenu;
+        private void Update()
+        {
+            if (!Input.GetKeyDown(KeyCode.Escape)) return;
+            if (IsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+
+        public void ResumeGame()
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            IsPaused = false;
+        }
+
+        public void GoToMenu()
+        {
+            Time.timeScale = 1f;
+            IsPaused = false;
+            pauseMenu.SetActive(false);
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        
+        public void PauseGame()
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            IsPaused = true;
+        }
+    }
+}
---
+++ Resist the Night/Assets/Scripts/MusicScripts/MusicPaused.cs
@@ -0,0 +1,26 @@
+﻿using MenuScripts;
+using UnityEngine;
+
+namespace MusicScripts
+{
+    public class MusicPaused : MonoBehaviour
+    {
+        private AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+        private void Update()
+        {
+            if (PauseMenu.IsPaused)
+            {
+                audioSource.volume = 0;
+            }
+            else
+            {
+                audioSource.volume = 1;
+            }
+        }
+    }
+}
