d67573d64c269e6fc4c32aa77defeeba6b97dcc8
Stefan247 <34254467+Stefan247@users.noreply.github.com>
2020-07-30 14:31:09 +0300

    Removed music

--- Resist the Night/Assets/Scripts/MenuScripts/SettingsMenuScript.cs
+++ Resist the Night/Assets/Scripts/MenuScripts/SettingsMenuScript.cs
@@ -1,18 +1,14 @@
 ﻿using UnityEngine;
-using UnityEngine.Audio;
 
 namespace MenuScripts
 {
     public class SettingsMenuScript : MonoBehaviour
     {
-        public AudioMixer audioMixer;
+        public AudioSource audioSource;
 
         private void SetVolume(float volume)
         {
-            audioMixer.SetFloat("volumeParameter", volume);
-            /*
-             *  Not being used yet.
-             */
+            audioSource.volume = volume;
         }
     }
 }
